26e827b518d87e6b610a65f106444efdb373e017
Yuan Wang <github@yuanagain.com>
2022-10-26 23:56:39 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write('Hello World! Blah blah');
   res.end();
 }).listen(process.env.PORT);
